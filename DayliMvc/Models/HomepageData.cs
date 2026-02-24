7806835ef411f8694a78ed7a430451707d3f945b
blomjord <andre_kramfors@hotmail.com>
2026-02-24 13:01:26 +0100

    Changed return value

--- DayliMvc/Models/HomepageData.cs
+++ DayliMvc/Models/HomepageData.cs
@@ -1,10 +1,11 @@
 using DayliMvc.Models.WeatherData;
 using DayliMvc.Models.CalendarData;
+using Google.Apis.Calendar.v3.Data;
 
 namespace DayliMvc.Models;
 
 public class HomepageData
 {
     public required WeatherDataFront Weather { get; set; }
-    public required CalendarDataFront Calendar { get; set; }
+    public List<Event> Calendar { get; set; } = new();
 }
