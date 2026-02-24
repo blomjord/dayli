6a7c0a6337a50085c275831ce7790ebb9c43d9a5
blomjord <andre_kramfors@hotmail.com>
2026-02-24 13:03:09 +0100

    Changed return value

--- DayliMvc/Models/CalendarData.cs
+++ DayliMvc/Models/CalendarData.cs
@@ -5,13 +5,7 @@ using Google.Apis.Calendar.v3.Data;
 namespace DayliMvc.Models.CalendarData;
 public class CalendarDataFront
 {
-    public List<Event> Events { get; set; } =  new();
+    public List<Event> Events { get; set; } = new();
 }
-
-public class CalendarDataPage
-{
-    public int? userId { get; set; }
-}
-
 
 
